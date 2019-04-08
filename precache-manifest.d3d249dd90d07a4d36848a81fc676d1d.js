4e6ebc2556f7f294254d28d54192c04b5ff5b87c
ptkmc <23647036+ptkmc@users.noreply.github.com>
2019-04-08 00:13:59 -0500

    Updates

---
+++ precache-manifest.d3d249dd90d07a4d36848a81fc676d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99a2803c1eca0848e320",
+    "url": "/test-page/static/js/runtime~main.c0cc131e.js"
+  },
+  {
+    "revision": "95d3820f3d750fde18fa",
+    "url": "/test-page/static/js/main.4c16757f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test-page/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "95d3820f3d750fde18fa",
+    "url": "/test-page/static/css/main.44341dbe.chunk.css"
+  },
+  {
+    "revision": "68bf3121555746adad48c667d5156ea1",
+    "url": "/test-page/index.html"
+  }
+];
